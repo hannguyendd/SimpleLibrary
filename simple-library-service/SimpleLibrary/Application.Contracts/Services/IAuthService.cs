2f2e5e9098b090fd780bc06a70d410c7734eae69
hannguyendd <han.nguyenex@gmail.com>
2024-06-21 23:26:35 +0700

    chore: remove unused files

--- simple-library-service/SimpleLibrary/Application.Contracts/Services/IAuthService.cs
+++
@@ -1,8 +0,0 @@
-using SimpleLibrary.Application.Contracts.Dtos.Authentication;
-
-namespace SimpleLibrary.Application.Contracts.Services;
-
-public interface IAuthService
-{
-    Task<TokenDto> LoginAsync(LoginRequest request);
-}
--- simple-library-service/SimpleLibrary/Application/AppicationService.cs
+++
@@ -1,8 +0,0 @@
-using SimpleLibrary.Domain.Repositories;
-
-namespace SimpleLibrary.Application;
-
-public abstract class ApplicationService(IUnitOfWork uow)
-{
-    protected readonly IUnitOfWork _uow = uow;
-}
